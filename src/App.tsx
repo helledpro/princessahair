4c323311fa01cc8d1acd1a0ec386fe761af52501
helledpro <helled.pro@gmail.com>
2026-08-25 16:13:33 +0300

    Mount timed conversion popup

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import {PostQuizBenefits} from './components/PostQuizBenefits';
 import {Quiz} from './components/Quiz';
 import {MainSections} from './components/Sections';
 import {MobileStickyCTA} from './components/Common';
+import {TimedPopup} from './components/TimedPopup';
 
 export function App(){
-  return <main><Hero/><Quiz/><PostQuizBenefits/><MainSections/><MobileStickyCTA/></main>
+  return <main><Hero/><Quiz/><PostQuizBenefits/><MainSections/><MobileStickyCTA/><TimedPopup/></main>
 }
