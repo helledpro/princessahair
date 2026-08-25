c4a738d0d1960aea15002d405e8178b8acef7621
helledpro <helled.pro@gmail.com>
2026-08-25 09:52:30 +0300

    Load conversion quiz styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,4 +3,5 @@ import {createRoot} from 'react-dom/client';
 import {App} from './App';
 import './styles.css';
 import './hero-fix.css';
+import './quiz-conversion.css';
 createRoot(document.getElementById('root')!).render(<React.StrictMode><App/></React.StrictMode>);
