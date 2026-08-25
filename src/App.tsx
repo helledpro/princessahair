77b99361c8cac67284606168da8358e8c351a7f8
helledpro <helled.pro@gmail.com>
2026-08-25 14:49:34 +0300

    Render mobile sticky CTA

--- src/App.tsx
+++ src/App.tsx
@@ -1,2 +1,9 @@
-import {Hero} from './components/Hero';import {PostQuizBenefits} from './components/PostQuizBenefits';import {Quiz} from './components/Quiz';import {MainSections} from './components/Sections';
-export function App(){return <main><Hero/><Quiz/><PostQuizBenefits/><MainSections/></main>}
+import {Hero} from './components/Hero';
+import {PostQuizBenefits} from './components/PostQuizBenefits';
+import {Quiz} from './components/Quiz';
+import {MainSections} from './components/Sections';
+import {MobileStickyCTA} from './components/Common';
+
+export function App(){
+  return <main><Hero/><Quiz/><PostQuizBenefits/><MainSections/><MobileStickyCTA/></main>
+}
