60b01aea07cc47f0a26523cfbe665629f7b9f52e
helledpro <helled.pro@gmail.com>
2026-09-21 12:47:41 +0300

    Switch Telegram contact to new phone number

--- src/components/Common.tsx
+++ src/components/Common.tsx
@@ -13,8 +13,8 @@ const photoPositions:Record<string,CSSProperties['objectPosition']>={
 
 const contactMessage='Здравствуйте! Хочу подобрать волосы Princessahair.';
 const encodedContactMessage=encodeURIComponent(contactMessage);
-const telegramProfileUrl='https://t.me/Princessahair_Studio';
-const telegramMobileUrl=`https://t.me/Princessahair_Studio?text=${encodedContactMessage}`;
+const telegramProfileUrl='https://t.me/+79650650790';
+const telegramMobileUrl=`https://t.me/+79650650790?text=${encodedContactMessage}`;
 const maxProfileUrl='https://max.ru/u/f9LHodD0cOLI7eBgRW67C5HWZZQB128y7nKqu4YltKxZrV578bHWW_s2Td8';
 
 function copyContactMessage(){
