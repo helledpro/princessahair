4098df8d586f55b309900395d0209be7256cc7a8
helledpro <helled.pro@gmail.com>
2026-09-21 12:44:46 +0300

    Remove unused social proof styles

--- src/main.tsx
+++ src/main.tsx
@@ -9,5 +9,4 @@ import './mobile-sticky.css';
 import './legal.css';
 import './timed-popup.css';
 import './assortment-gallery.css';
-import './social-proof.css';
 createRoot(document.getElementById('root')!).render(<React.StrictMode><App/></React.StrictMode>);
